f15fb8a1539a5ce2d2d0899e1e1e8d17d5f69d1e
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-07-13 16:17:54 +0700

    chìu 13/7

--- WEB/JS/Home.js
+++ WEB/JS/Home.js
@@ -1,13 +1,45 @@
 
-function changeLeft(){
-    var s = document.getElementById("stories")
-    var l=0;
-    l+=50;
-    s.style.left=l+"px"
+function changeLeft() {
+    let a = document.getElementById("stories");
+    let d = 50;
+    let computedStyle = window.getComputedStyle(a);
+    let leftValue = computedStyle.getPropertyValue("left");
+    let rightValue = computedStyle.getPropertyValue("right");
+    
+    let left1 =  parseInt(leftValue, 10);
+    let right1 =  parseInt(rightValue, 10);
+    if(right1>=0){
+        let newleft = left1 + d;
+    a.style.left = newleft+"px";
+    }
+    
 };
-function changeRight(){
-    var s = document.getElementById("stories")
-    var l=0;
-    l-=50;
-    s.style.left=l+"px"
-};
+
+
+function changeRight() {
+    let a = document.getElementById("stories");
+    let d = 50;
+    let computedStyle = window.getComputedStyle(a);
+    let leftValue = computedStyle.getPropertyValue("left");
+    let rightValue = computedStyle.getPropertyValue("right");
+
+    let left1 =  parseInt(leftValue, 10);
+    let right1 =  parseInt(rightValue, 10);
+    if(right1<=0){
+        let newleft = left1 - d;
+        a.style.left = newleft+"px";
+    }
+}
+
+// function changeLeft(){
+//     var s = document.getElementById("stories")
+//     var l=0;
+//     l+=50;
+//     s.style.left=l+"px"
+// };
+// function changeRight(){
+//     var s = document.getElementById("stories")
+//     var l=0;
+//     l-=50;
+//     s.style.left=l+"px"
+// };
---
+++ WEB/JS/Profile.js
@@ -0,0 +1,9 @@
+function changeIcon(obj){
+    let icon = document.getElementsByTagName("span");
+    let a = obj.getElementsByTagName("i");
+    // a.setAttribute
+    
+    // icon.set
+    a.setAttribute("color","blue");
+    // alert(obj)
+}
