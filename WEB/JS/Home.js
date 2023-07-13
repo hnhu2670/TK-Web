12c92d654db4a6d4a6dc12ce94f7baa3d44c5a66
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-07-13 08:37:58 +0700

    13/7

---
+++ WEB/JS/Home.js
@@ -0,0 +1,13 @@
+
+function changeLeft(){
+    var s = document.getElementById("stories")
+    var l=0;
+    l+=50;
+    s.style.left=l+"px"
+};
+function changeRight(){
+    var s = document.getElementById("stories")
+    var l=0;
+    l-=50;
+    s.style.left=l+"px"
+};
