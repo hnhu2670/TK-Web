6ed9e27c46bb37af0b23e89835d75e0bba4d66dd
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-07-14 22:18:13 +0700

    14/7

--- WEB/JS/Profile.js
+++ WEB/JS/Profile.js
@@ -1,9 +1,23 @@
 function changeIcon(obj){
-    let icon = document.getElementsByTagName("span");
-    let a = obj.getElementsByTagName("i");
-    // a.setAttribute
+    // let icon = document.getElementsByTagName("span");
+    // let a = obj.getElementsByTagName("i");
+    // // a.setAttribute
     
-    // icon.set
-    a.setAttribute("color","blue");
-    // alert(obj)
+    // // icon.set
+    // a.setAttribute("color","blue");
+    // // alert(obj)
+    var icon = document.querySelectorAll(".icon-heart")
+    for (var i =0;i<=icon.length;i++)
+    {
+        icon[i].setAttribute=("color","blue");
+    }
 }
+
+function loadItems(){
+    fetch('/JSOM/items.json').then(res => res.json()).then(data => {
+        var h="";
+        
+    })
+    
+
+}    //console.info("thành công")
