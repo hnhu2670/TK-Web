348a9830970f1396f586434ca6298194287746c9
hnhu2670 <2051050327nhu@ou.edu.vn>
2023-07-26 08:52:05 +0700

    update

--- WEB/JS/Profile.js
+++ WEB/JS/Profile.js
@@ -1,42 +1,40 @@
-function init(){
+function init() {
     var a = document.querySelectorAll("div.icon div.icon-heart")
-    for(var i=0;i<a.length;i++)
-    
-        icon[i].onclick = function()
-        {
+    for (var i = 0; i < a.length; i++)
+
+        icon[i].onclick = function () {
             alert('hello')
         }
-    
+
 }
 
-$(window).on("load", function() {
-    $(document).ready(function(){
-              
+$(window).on("load", function () {
+    $(document).ready(function () {
+
         $(".sub").hide()
-    
-        $(window).scroll(()=>{
-            if($(this).scrollTop()>=600)
-            {
+
+        $(window).scroll(() => {
+            if ($(this).scrollTop() >= 600) {
                 $(".sub").show()
-    
+
                 // $(".sub").slideDown(500)
                 $(".sub").css({
-                    "position":"fixed",
-                    "z-index":9999
+                    "position": "fixed",
+                    "z-index": 9999
                 })
                 $(".sub-Profile").css({
-                    "background-color":"blue",
-    
+                    "background-color": "blue",
+
                 })
             }
-            else{
+            else {
                 $(".sub").hide()
             }
         })
     })
-  });
+});
 
-  function load_item() {
+function load_item() {
     let c = document.getElementById("items");
     c.innerHTML = "";
 
@@ -46,21 +44,51 @@ $(window).on("load", function() {
         }).then((data) => {
             let d = document.getElementById("items");
             for (i of data) {
-                
+
                 d.innerHTML +=
-            `<div class="item">
-                <img src="${i.img}" alt="">
-                <div class="bg-top ">
-                    <div class="flex icon">
-                        <div class="icon-heart">
-                            <i class="fa-solid fa-heart"></i>
+                    `<div class="item">
+                        <img src="${i.img}" alt="">
+                        <div class="bg-top ">
+                            <div class="flex icon">
+                                <div class="icon-heart">
+                                    <i class="fa-solid fa-heart"></i>
+                                </div>
+                                <div class="icon-share">
+                                    <i class="fa-regular fa-paper-plane "></i>
+                                </div>
+                            </div>
                         </div>
-                        <div class="icon-share">
-                            <i class="fa-regular fa-paper-plane "></i>
-                        </div>
-                    </div>
-                </div>
-            </div>`
-                        }
-                    })
+                    </div>`
             }
+        })
+}
+
+// function load_anh(obj) {
+//     let like = document.getElementById("like")
+//     like.style.display = "block"
+// }
+
+window.onload = function () {
+    let item = document.getElementById("item")
+    let like = document.getElementById("like")
+    let tag = document.getElementById("tag")
+    let ds = document.querySelectorAll(".list li")
+    console.log(ds)
+    // for (let i = 0; i < ds.length; i++)
+    //     ds[i].onclick = function () {
+    //         if (ds[i] == ds[0]) {
+    //             item.style.display = "block"
+    //         }
+    //         if (ds[i] == ds[1]) {
+    //             item.style.display = "none"
+    //             tag.style.display = "none"
+    //             like.style.display = "block"
+    //         }
+    //         if (ds[i] == ds[2]) {
+    //             like.style.display = "none"
+    //             item.style.display = "none"
+    //             tag.style.display = "block"
+
+    //         }
+    //     }
+}
